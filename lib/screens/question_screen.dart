4ac0a56223c4f4e10c60c0fb55ffd14049622e76
Dayoung331 <pinga331@hanyang.ac.kr>
2024-07-19 20:46:17 +0900

    탭 세 개

---
+++ lib/screens/question_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Question Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
