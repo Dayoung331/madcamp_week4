2ebb1eddd9da6989d76063dbb4ad7940906f17f6
Dayoung331 <pinga331@hanyang.ac.kr>
2024-07-19 21:19:16 +0900

    질문 탭

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Bottom Navigation',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: MainScreen(),
     );
   }
--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -3,10 +3,66 @@ import 'package:flutter/material.dart';
 class QuestionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Question Screen',
-        style: TextStyle(fontSize: 24),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('2024. 07. 19'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // 뒤로가기 액션을 추가하세요.
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () {
+              // 앞으로 가기 액션을 추가하세요.
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Text(
+              '“오늘 하루 가장 기분 좋은 순간은 언제였나요?”',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 150,
+              child: TextField(
+                maxLines: null,
+                expands: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '답변',
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // 버튼 액션을 추가하세요.
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.grey, // 텍스트 색상
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              ),
+              child: Text('과거의 나 돌아보기'),
+            ),
+          ],
+        ),
       ),
     );
   }
