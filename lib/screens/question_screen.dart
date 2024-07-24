c68cc3fa7b4f3b406b9e20fc2021c3aa8654ad9a
seungju lee <dltmdwn0322@gmail.com>
2024-07-24 20:26:18 +0900

    디자인 컨셉 블랙으로 수정

--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -139,54 +139,116 @@ class _QuestionScreenState extends State<QuestionScreen> {
   }
 
   void _showConfirmationDialog(String dateKey, String newAnswer) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('답변을 다시 제출하시겠습니까?'),
-          content: Text('기존 답변이 덮어씌워집니다. 계속하시겠습니까?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('아니요'),
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
             ),
-            TextButton(
-              onPressed: () {
-                _saveAnswer(dateKey, newAnswer);
-                _answerController.clear();
-                setState(() {
-                  _isAnswerSubmitted = true;
-                });
-                Navigator.of(context).pop();
-                print("Answer resubmitted: $newAnswer for date: $dateKey");
-                _showSnackbar("답변이 제출되었습니다!"); // Snackbar 표시
-                _showRevealAnimation(); // 애니메이션 실행
-              },
-              child: Text('예'),
+            backgroundColor: Colors.white, // 배경색을 흰색으로 설정
+            titlePadding: EdgeInsets.all(0),
+            contentPadding: EdgeInsets.all(0),
+            actionsPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            content: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              decoration: BoxDecoration(
+                color: Colors.white, // 배경색을 흰색으로 설정
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.error_outline,
+                    color: Colors.black,
+                    size: 40,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    '기존 답변이 덮어씌워집니다. 계속하시겠습니까?',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'NotoSerifKR',
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        );
-      },
-    );
+            actions: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey[200],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 12),
+                        ),
+                        child: Text(
+                          '취소',
+                          style: TextStyle(
+                            fontFamily: 'NotoSerifKR',
+                            color: Colors.black,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 10), // 버튼 간격 조정
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 12),
+                        ),
+                        child: Text(
+                          '예',
+                          style: TextStyle(
+                            fontFamily: 'NotoSerifKR',
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          _saveAnswer(dateKey, newAnswer);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      );
   }
 
   void _showSnackbar(String message) {
     final snackBar = SnackBar(
-        content: Row(
-          children: [
-            Icon(Icons.check_circle, color: Color(0xFFE5D0B5)),
-            SizedBox(width: 8.0),
-            Text(message),
-          ],
-        ),
-        backgroundColor: Colors.grey[600],
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        duration: Duration(seconds: 2),
+      content: Row(
+        children: [
+          Icon(Icons.check_circle, color: Color(0xFFE5D0B5)),
+          SizedBox(width: 8.0),
+          Text(message),
+        ],
+      ),
+      backgroundColor: Colors.grey[600],
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      duration: Duration(seconds: 2),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
@@ -202,41 +264,21 @@ class _QuestionScreenState extends State<QuestionScreen> {
     }
   }
 
-  void _showEditQuestionDialog(int dayOfYear) {
-    TextEditingController questionController = TextEditingController(text: _questions[dayOfYear - 1]);
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('질문 수정'),
-          content: TextField(
-            controller: questionController,
-            decoration: InputDecoration(
-              hintText: '질문을 입력하세요',
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('취소'),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  _questions[dayOfYear - 1] = questionController.text;
-                });
-                _saveQuestions();
-                Navigator.of(context).pop();
-                _showSnackbar("질문이 수정되었습니다!");
-              },
-              child: Text('저장'),
-            ),
-          ],
-        );
-      },
+  void _navigateToEditQuestionScreen(int dayOfYear, String question) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditQuestionScreen(dayOfYear: dayOfYear, question: question),
+      ),
     );
+
+    if (result != null && result is String) {
+      setState(() {
+        _questions[dayOfYear - 1] = result;
+      });
+      _saveQuestions();
+      _showSnackbar("질문이 수정되었습니다!");
+    }
   }
 
   @override
@@ -293,19 +335,19 @@ class _QuestionScreenState extends State<QuestionScreen> {
         child: Stack(
           children: [
             SingleChildScrollView(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start, // 상단 정렬
                 children: [
                   Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 16.0),
-                      child: Transform.rotate(
-                        angle: 180 * 3.1415927 / 180,
-                        child: Icon(Icons.format_quote, size: 30, color: Colors.black),
-                      ),
-                    )
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 16.0),
+                        child: Transform.rotate(
+                          angle: 180 * 3.1415927 / 180,
+                          child: Icon(Icons.format_quote, size: 30, color: Colors.black),
+                        ),
+                      )
                   ),
                   Container(
                     width: 250,
@@ -329,16 +371,18 @@ class _QuestionScreenState extends State<QuestionScreen> {
                   ),
                   IconButton(
                     icon: Icon(Icons.edit, size: 20),
+                    padding: EdgeInsets.zero,
+                    constraints: BoxConstraints(),
                     onPressed: () {
-                      _showEditQuestionDialog(dayOfYear);
-                      },
+                      _navigateToEditQuestionScreen(dayOfYear, question);
+                    },
                   ),
                   Align(
-                    alignment: Alignment.centerRight,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 16.0),
-                      child: Icon(Icons.format_quote, size: 30, color: Colors.black),
-                    )
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 16.0),
+                        child: Icon(Icons.format_quote, size: 30, color: Colors.black),
+                      )
                   ),
                   SizedBox(height: 20),
                   Container(
@@ -350,7 +394,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
                     ),
                     child: TextField(
                       controller: _answerController,
-                      maxLines: 10,
+                      maxLines: 9,
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: '답변',
@@ -365,8 +409,9 @@ class _QuestionScreenState extends State<QuestionScreen> {
                     onPressed: _submitAnswer,
                     style: ElevatedButton.styleFrom(
                       foregroundColor: Colors.white,
-                      backgroundColor: _isAnswerSubmitted ? Colors.green : Colors.grey,
-                      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10), // 버튼 크기 조정
+                      backgroundColor: _isAnswerSubmitted ? Color(0xFF252525) : Color(0xFF979797),
+                      padding: EdgeInsets.symmetric(horizontal: 112, vertical: 10), // 버튼 크기 조정
+                      minimumSize: Size(250, 50),
                     ),
                     child: Row(
                       mainAxisSize: MainAxisSize.min,
@@ -402,7 +447,7 @@ class _QuestionScreenState extends State<QuestionScreen> {
                     },
                     style: ElevatedButton.styleFrom(
                       foregroundColor: Colors.white,
-                      backgroundColor: Colors.grey,
+                      backgroundColor: Color(0xFF979797),
                       padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
                       minimumSize: Size(250, 50), // 버튼의 최소 크기 설정
                     ),
@@ -429,6 +474,76 @@ class _QuestionScreenState extends State<QuestionScreen> {
         ),
       ),
       resizeToAvoidBottomInset: true,
+    );
+  }
+}
+
+class EditQuestionScreen extends StatelessWidget {
+  final int dayOfYear;
+  final String question;
+  final TextEditingController _controller;
+
+  EditQuestionScreen({required this.dayOfYear, required this.question}) : _controller = TextEditingController(text: question);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        leading: // 왼쪽 패딩 추가
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('취소', style: TextStyle(color: Colors.black, fontFamily: 'AppleMyungjo')),
+        ),
+        actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(_controller.text);
+              },
+              child: Text('완료', style: TextStyle(color: Colors.black, fontFamily: 'AppleMyungjo')),
+            ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 230),
+                  Text(
+                    '원하는 질문으로 바꿔보세요',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                      fontFamily: 'AppleMyungjo',
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  TextField(
+                    controller: _controller,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 24, color: Colors.black, fontFamily: 'AppleMyungjo'),
+                    maxLines: null, // 여러 줄에 걸쳐서 텍스트를 표시할 수 있도록 설정
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '질문을 입력하세요',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
